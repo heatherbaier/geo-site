a9a7d519d0671073f8909b31219f5f74d8d8af08
heatherbaier <hmbaier@email.wm.edu>
2022-05-05 23:41:41 -0400

    slightly better landing page

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	<img class="index-image" src="assets/index_image.jpeg"/>
 	<span id="overlay_text">
 		<div class="center-index">
-			<h1>Welcome to the Global Education Observatory</h1>
-			<h3>Vestibulum vel nulla et libero ullamcorper rhoncus eget et tellus. Cras id est fringilla, ultricies dui vitae, tincidunt tortor. Etiam vel pharetra mi. Morbi auctor, lorem at faucibus porttitor, nibh orci eleifend ipsum, vel venenatis turpis nisi id dui. Suspendisse congue eu ligula vel sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer lorem neque, posuere id lorem quis, bibendum congue leo. Nam fringilla eu diam quis tincidunt. Aliquam consequat augue eu augue sodales, sed luctus elit congue. Suspendisse sem nunc, efficitur et efficitur at, scelerisque efficitur purus.</h3>
+			<h1  style="font-family: 'Montserrat', sans-serif; width: 700px;" >Welcome to the Global Education Observatory</h1>
+			<h3  style="font-family: 'Montserrat', sans-serif; font-size: 18px; width: 700px;" >Vestibulum vel nulla et libero ullamcorper rhoncus eget et tellus. Cras id est fringilla, ultricies dui vitae, tincidunt tortor. Etiam vel pharetra mi. Morbi auctor, lorem at faucibus porttitor, nibh orci eleifend ipsum, vel venenatis turpis nisi id dui. Suspendisse congue eu ligula vel sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer lorem neque, posuere id lorem quis, bibendum congue leo. Nam fringilla eu diam quis tincidunt. Aliquam consequat augue eu augue sodales, sed luctus elit congue. Suspendisse sem nunc, efficitur et efficitur at, scelerisque efficitur purus.</h3>
 			<div class="index-buttons">
 				<a href="login.php"><button class="index-button">Login</button></a>
 				<a href="register.php"><button class="index-button">Register</button></a>
