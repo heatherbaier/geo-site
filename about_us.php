be7f129813d228619cbd60789253963c29e373b9
courtney-maynard <89987853+courtney-maynard@users.noreply.github.com>
2022-11-23 17:50:04 -0500

    updates to about us page
    added past members and fixed some styling errors

--- about_us.php
+++ about_us.php
@@ -1,11 +1,13 @@
 <?php include 'includes/aboutus_header.php' ?>
 
 <div id = 'aboutUsPage'>
-    <h2 id ='aboutPageHeader' > Meet the past and present members of the GEO team: </h2>
+    <!-- <h2 id ='aboutPageHeader' > Meet the past and present members of the GEO team: </h2> -->
 
-    <div id = "heatherSection">
+    <h3 class = 'memHeader'> Current Members <h3>
+
+    <div class = "personalSection">
         <img class = "aboutPictures" src = "assets/fillerpic.png">
-        <div id = "heatherDescription">
+        <div class = "personalDescription">
             <h6 class = "aboutHeaders"> Heather Baier, W&M PhD Student, Computational Geography </h6>
             <p class = "aboutDescriptions"> 
                 filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text filler text 
@@ -13,9 +15,9 @@
         </div>
     </div>
 
-    <div id = "janeSection">
+    <div class = "personalSection">
         <img class = "aboutPictures" src = "assets/fillerpic.png">
-        <div id = "janeDescription">
+        <div class = "personalDescription"">
             <h6 class = "aboutHeaders"> Jane Siwek, W&M 2023, CAMS Mathematical Biology </h6>
             <p class = "aboutDescriptions"> 
             Jane Siwek is one of the student co-directors for GEO and is an undergraduate student graduating from W&M in May 2023 with a degree in Computational & Applied Mathematics & Statistics, concentrating in Mathematical Biology. Previously, she used deep learning models to predict school scores using satellite imagery and analyzed education data, and currently she is geocoding for school coordinates. Her other research interests include using natural language processing and machine learning to analyze mutations in protein-protein interactions.
@@ -23,9 +25,9 @@
         </div>
     </div>
 
-    <div id = "naomiSection">
+    <div class = "personalSection">
         <img class = "aboutPictures" src = "assets/fillerpic.png">
-        <div id = "naomiDescription"> 
+        <div class = "personalDescription"> 
             <h6 class = "aboutHeaders"> Naomi Levin, W&M 2023, Biology </h6>
             <p class = "aboutDescriptions"> 
             Naomi Levin cleans data for GEO and is pursuing a B.S. in biology as a pre-med student who will graduate from W&M in May 2023. Aside from her research with geoLab, Naomi’s research interests include kinesthetics related to osseointegration and limb salvage/amputation. 
@@ -33,9 +35,9 @@
         </div>
     </div>
 
-    <div id = "courtneySection">
+    <div class = "personalSection">
         <img class = "aboutPictures" src = "assets/fillerpic.png">
-        <div id = "courtneyDescription">
+        <div class = "personalDescription">
             <h6 class = "aboutHeaders"> Courtney Maynard, W&M 2025, Data Science </h6>
             <p class = "aboutDescriptions">
             Courtney Maynard coded and maintains the map of schools for the GEO website. Additionally, she built the “Home” and “About Us” pages. She is a James Monroe Scholar graduating in May 2025 with a major in Data Science and a minor in Psychology. Her research interests outside of the geoLab include analyzing the concurrent appearance of emotion with sensemaking about data, as seen through elementary school students’ exploration of data science, as well as ethical data collection, usage, and publication. 
@@ -43,9 +45,9 @@
         </div>
     </div>
 
-    <div id = "annamarieSection">
+    <div class = "personalSection">
         <img class = "aboutPictures" src = "assets/fillerpic.png">
-        <div id = "annamarieDescription">
+        <div class = "personalDescription">
             <h6 class = "aboutHeaders"> Annamarie Warnke, W&M 2025, Data Science </h6>
             <p class = "aboutDescriptions">
             Annamarie Warnke writes blog posts for GEO. They are an undergraduate James Monroe Scholar graduating from W&M in May 2025. They are a Data Science major and intended Educational Studies minor. Their other research interests include higher education policy and K-12 data science and computer science education. 
@@ -53,9 +55,9 @@
         </div>
     </div>
 
-    <div id = "kaitlynSection">
+    <div class = "personalSection">
         <img class = "aboutPictures" src = "assets/fillerpic.png">
-        <div id = "kaitlynDescription">
+        <div class = "personalDescription">
             <h6 class = "aboutHeaders"> Kaitlyn Wilson, W&M 2023, Data Science and Government </h6>
             <p class = "aboutDescriptions">
             Kaitlyn Wilson collects education data and creates data visualizations for GEO. She is an undergraduate student graduating from W&M in May 2023 with majors in Data Science and Government. 
@@ -63,7 +65,71 @@
         </div>
     </div>
 
+    <h3 class = 'memHeader'> Past Members <h3>
 
+    <div>
+        <div class = 'pastMemRow'>
+            <div class = "pastPersonalDescription">
+                <h5 class = "pastHeaders"> Elias Wolman </h5>
+                <p class = "pastDescriptions"> 
+                Elias Wolman was the director of GEO and graduated from W&M in May 2022 with a major in Computational and Applied Mathematics and Statistics and a minor in German Studies. He was with the team since Spring 2020 and his research interests include education and sports analytics.
+                </p>
+            </div>
+
+            <div class = "pastPersonalDescription">
+                <h5 class = "pastHeaders"> Jiaying(Tina) Chen </h5>
+                <p class = "pastDescriptions"> 
+                Jiaying(Tina) Chen worked on the database and its related features for the orginial website of GEO. She graduated from W&M in May 2022 with degrees in Computer Science and Applied Mathematics. Her research interests include the applications of neural networks, statistical modeling, and computational topology on geospatial data. 
+                </p>
+            </div>
+
+            <div class = "pastPersonalDescription">
+                <h5 class = "pastHeaders"> Kerry Wang </h5>
+                <p class = "pastDescriptions"> 
+                Kerry Wang was a James Monroe Scholar at William & Mary and the web developer of GEO. He graduated from W&M in May 2022 with degrees in Computer Science and Physics. Besides web development, his interests include machine learning and systems administration. 
+                </p>
+            </div>
+        </div>
+        
+        <div class = 'pastMemRow'>
+            <div class = "pastPersonalDescription">
+                <h5 class = "pastHeaders"> Jeremy Swack </h5>
+                <p class = "pastDescriptions"> 
+                Jeremy Swack worked on data analytics projects for GEO and is an undergraduate student graduating from W&M in May 2024. He plans to double major in Data Science and Computer Science. His other research interests include machine learning applications in sports analytics and with geospatial data.  
+                </p>
+            </div>
+
+            <div class = "pastPersonalDescription">
+                <h5 class = "pastHeaders"> Alison Reynolds </h5>
+                <p class = "pastDescriptions"> 
+                Alison Reynolds was the director of GEO and graduated from W&M in May 2021 with degrees in Mathematics and Data Science. Her research interests include data visualization and using machine learning to analyze education and weather data.
+                </p>
+            </div>
+
+            <div class = "pastPersonalDescription">
+                <h5 class = "pastHeaders"> John Hennin </h5>
+                <p class = "pastDescriptions"> 
+                John Hennin collected education data for GEO and graduated from W&M in May 2022 with a degree in Data Science. In addition to GEO, he researched machine learning applications for identifying road quality and predicting county-level opioid overdose rates.
+                </p>
+            </div>
+        </div>
+
+        <div class = 'pastMemRow'>
+            <div class = "pastPersonalDescription">
+                <h5 class = "pastHeaders"> Caroline Edwards </h5>
+                <p class = "pastDescriptions"> 
+                Caroline Edwards worked on data cleaning for GEO and is an undergraduate student graduating from W&M in May 2023 with a degree in Data Science and Kinesiology. Her research interests include using geospatial data to analyze issues related to public health and environmental science.
+                </p>
+            </div>
+
+            <div class = "pastPersonalDescription">
+                <h5 class = "pastHeaders"> Fatima Pate </h5>
+                <p class = "pastDescriptions"> 
+                Fatima Pate collected education data for GEO. She is a fourth year Data Science major on the Pre- Med track. Her interests outside of the lab include analyzing the benefits of machine learning in health care systems of developing countries. 
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
 
 </body>
